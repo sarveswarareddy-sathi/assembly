b6f6349d2aa1908c8b8d742f5147214145a69173
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-05-19 17:42:28 +0800

    C to x86 disassembly

--- 04.19_C_x86_integers/04.19_C_x86_integers.asm
+++ 04.19_C_x86_integers/04.19_C_x86_integers.asm
@@ -1,4 +1,21 @@
 %include "asm_io.inc"
+; Standard Types - Integers, Long, Shorts, Bytes ---> How they manifest in assembly?
+; standard int library - uint16_t in stdlib.h
+; Integers - DWORD size --> int & unsigned
+; while assigning one variable to another --> movzx/movsx
+; lowest/smallest byte is zero extended
+;
+; SHORT - keyword WORD
+;
+; Long - 4-8 byte data types - 32-bit on 32bit C, 64-bit one 64bit c
+; long long - 64-bits on both guarantee
+; Usually DONE by two DWORD copies
+;
+; 64-bit uses QWORD for 64-bit
+;
+; char --> BYTE char/int8/uint8
+;char and int8_t treated same
+;
 segment .data 
 
 segment .bss 
