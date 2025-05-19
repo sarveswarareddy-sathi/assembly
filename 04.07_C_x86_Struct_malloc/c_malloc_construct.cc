93f32fcbb7190522f7204d2d82c946f911c103b6
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-05-19 10:40:37 +0800

    Create c_malloc_construct.cc

---
+++ 04.07_C_x86_Struct_malloc/c_malloc_construct.cc
@@ -0,0 +1,11 @@
+//Allocating via SOCKET
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[]) {
+    struct sockaddr_in * serv_addr = malloc(sizeof(struct sockaddr_in));
+}
