4c2d6bfd5d3ab22016ffd4511ca445f590328d02
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-05-19 10:40:03 +0800

    C to x86 disassembly

--- 04.07_C_x86_Struct_malloc/c_malloc.cc
+++ 04.07_C_x86_Struct_malloc/c_malloc.cc
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-    int *x = malloc(sizeof(int));
+    int *x = (int * )malloc(sizeof(int));//allocate size of one integer - basically allocating in local space
     int size = 100;
-    int *xarray = malloc(sizeof(int) * size);
+    int *xarray = (int * )malloc(sizeof(int) * size);//allocate size of array
     xarray[99] = -1;
 }
