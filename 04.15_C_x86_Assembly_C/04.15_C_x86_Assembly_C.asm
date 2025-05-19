b0101d979b94822742351cb820d88e0979a11dcc
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-05-19 15:26:34 +0800

    C to x86 disassembly

--- 04.15_C_x86_Assembly_C/04.15_C_x86_Assembly_C.asm
+++ 04.15_C_x86_Assembly_C/04.15_C_x86_Assembly_C.asm
@@ -1,17 +1,41 @@
 %include "asm_io.inc"
+; Using Assembly and C together - USING nasm generated obj file and use in GCC c-language
+; GCC & NASM ------------
+; Nasm - generate an object file (.o)
+; Nasm code - global functionName
+; GCC - can take as input object files to create an output file
+; Last file has the main method
+; Function declaration - type functionName (type1 arg1,..., typen argn);
+;
+; Problem ---------
+; write a string length function
+; create the function in an assembly file - int lenstring(char *c, int max);
+; Use the function in a C file
+;
 segment .data 
 
 segment .bss 
 
 segment .text
-        global  asm_main
-asm_main:
+        global  lenstring
+lenstring:
         enter   0,0               ; setup routine
-        pusha
-	;***************CODE STARTS HERE***************************
-		
-	;***************CODE ENDS HERE*****************************
-        popa
-        mov     eax, 0            ; return back to C
+
+        push edi
+        push ecx
+
+        mov eax, 0
+        mov edi, [ebp + 8]
+        mov ecx, [ebp + 0Ch]
+        cld
+        repne scansb
+
+        mov ecx, [ebp + 8]
+        mov eax, edi
+        sub eax, ecx
+        dec eax
+
+        push edi
+        push ecx
         leave                     
         ret
