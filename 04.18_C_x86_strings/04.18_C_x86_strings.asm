1261d8a684ae6de48966c8497219db28c63e5021
Sarveswara Reddy Sathi <79434955+sarveswarareddy-sathi@users.noreply.github.com>
2025-05-19 15:29:23 +0800

    C to x86 disassembly

--- 04.18_C_x86_strings/04.18_C_x86_strings.asm
+++ 04.18_C_x86_strings/04.18_C_x86_strings.asm
@@ -1,4 +1,10 @@
 %include "asm_io.inc"
+; Strings in C in assembly - how on the stack, in the data/bss section & on the heap
+; C strings - C allws string to allocated several different ways
+; Examples - within a function, static & using malloc
+; printable characters use lowest 7 bits
+; NuLL terminator
+;
 segment .data 
 
 segment .bss 
